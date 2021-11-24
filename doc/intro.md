7a8a0b999adca91389acd136460efc45255f7eca
rafael <rafael_oliveira500@hotmail.com>
2021-11-23 23:24:36 -0300

    feat: add initial project files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-finance
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_finance/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-finance.core-test
+  (:require [clojure.test :refer :all]
+            [clj-finance.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
