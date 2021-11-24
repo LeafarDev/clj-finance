dacadb06c304b84746325e5d74916e121e428ae1
rafael <rafael_oliveira500@hotmail.com>
2021-11-24 20:55:11 -0300

    lazy infinite

--- src/clj_finance/core.clj
+++ src/clj_finance/core.clj
@@ -1,6 +1,13 @@
 (ns clj-finance.core
   (:gen-class))
 
+(defn generate-prices [lower-bound upper-bound]
+  (filter (fn [x] (>= x lower-bound))
+          (repeatedly (fn [] (rand upper-bound)))))
+
+;;(take 1000000 (generate-prices 50 1000000000000000000))
+(def pricelist (c/generate-prices 12 35))
+;; (take 25 (map (fn [x] {:price x}) pricelist))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
